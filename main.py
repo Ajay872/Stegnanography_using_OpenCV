c8fc2c3cba42cddfbe649829c15b00d145fddbf4
Paidi Ajay <ajaypaidi872@gmail.com>
2022-07-29 14:31:35 +0530

    Embedded and extracted the information using image
    Added an image now to the program and then Embedded  a string information through the image and extracted the information from the image and read the string successfully

--- main.py
+++ main.py
@@ -1,48 +1,102 @@
-#split the byte into bits
-#def split(data): #data: 104 ---> [3,2,0]
-    #extract last 2 bits
-    #blue_bits = data & 0x3
-    #lose last 2 bits
-    #data  = data >> 2
-    #extra 3 bits
-    #green_bits = data & 0x7
-    #lose last 3 bits
-    #red_bits = data >> 3
-    #return [red_bits, green_bits, blue_bits]
-    #return [data >> 5, (data>>2)& 0x7, data & 0x3]
+import cv2 #pip install opencv-python
 
-
-#l = split(ord('h'))
-#print(l)
-
-# def extract_bits(band, n):
-#     return band & (2**n -1)
-
-#def merge_bits(rbits, gbits, bbits):
-    #data = rbits
-    #data = data << 3 # make room for 3 bits
-    #data = data | gbits # merge the gbits
-    #data = data << 2 #make room for 2 bits
-    #data = data | bbits
-    #return data
-    #return (((rbits<< 3) | gbits) << 2) | bbits
-
-split = lambda data: [data >> 5, (data>>2)& 0x7, data & 0x3]
-extract_bits = lambda band, n : band & (2**n-1)
+split_byte_to_bits = lambda data: [data >> 5, (data>>2)& 0x7, data & 0x3]
+extract_nbits_of_byte = lambda band, n : band & (2**n-1)
 merge_bits = lambda rbits,gbits, bbits :(((rbits<< 3) | gbits) << 2) | bbits
 
-def main():
-    print('Enter data (0-255) : ')
-    x = int(input()) #104 (h)
-    print(x)
-    bits = split(x)
-    print(bits)
+def embed(vessel_image, string, target_image):
+    #load the image in memory
+    mem_image = cv2.imread(vessel_image, cv2.IMREAD_COLOR)
+    #type(mem_image) --> numpy.ndarray
+    #mem_image.shape --> height, width, pixelsize(bgr)
+    h,w,_ = mem_image.shape
 
-    #extract
-    a = extract_bits(bits[0], 3)
-    b = extract_bits(bits[1], 3)
-    c = extract_bits(bits[2], 2)
-    data = merge_bits(a,b,c)
-    print(data, chr(data))
+    index = 0
+    max_size = len(string)
+
+    #reading the pixels of the image
+    for i in range(h): #for each row
+        if index == max_size:
+            break
+
+        for j in range(w): #for each col of the row
+            if index == max_size:
+                break
+            #fetch a pixel
+            pixel = mem_image[i,j]
+            blue = pixel[0]
+            green = pixel[1]
+            red = pixel[2]
+
+            #fetch a character from the string
+            ch = string[index]
+            bits = split_byte_to_bits(ord(ch))
+
+            #free the last 3 bits of red and green
+            red = red & (~0x7)
+            green = green & (~0x7)
+            # free the last 2 bits of blue
+            blue = blue & (~0x3)
+
+            #merge
+            red = red | bits[0]
+            green = green | bits[1]
+            blue = blue | bits[2]
+
+            #update the mem_image_pixel[i,j]
+            mem_image[i, j, 0] = blue
+            mem_image[i, j, 1] = green
+            mem_image[i, j, 2] = red
+
+            index+=1
+
+    #save back
+    cv2.imwrite(target_image, mem_image)
+
+def extract(image_with_embedding):
+    #load the image in memory
+    mem_image = cv2.imread(image_with_embedding, cv2.IMREAD_COLOR)
+    #type(mem_image) --> numpy.ndarray
+    #mem_image.shape --> height, width, pixelsize(bgr)
+    h,w,_ = mem_image.shape
+
+    index = 0
+    max_size = 26 #len(string)
+    string = ''
+
+    #reading the pixels of the image
+    for i in range(h): #for each row
+        if index == max_size:
+            break
+
+        for j in range(w): #for each col of the row
+            if index == max_size:
+                break
+            #fetch a pixel
+            pixel = mem_image[i,j]
+            blue = pixel[0]
+            green = pixel[1]
+            red = pixel[2]
+
+            #fetch the last 3 bits of red and green
+            red_bits = red & 0x7
+            green_bits = green & 0x7
+            # fetch the last 2 bits of blue
+            blue_bits = blue & 0x3
+
+            #merge the bits to form a byte
+            ch = chr(merge_bits(red_bits, green_bits, blue_bits))
+            string = string + ch
+
+            index+=1
+
+    return string
+
+
+def main():
+    embed('C:/Users/DELL/OneDrive/Desktop/Stegnanography/Stegnanography_using_OpenCV/e_kids.png', 'This is a sample sentence!', 'C:/Users/DELL/OneDrive/Desktop/Stegnanography/Stegnanography_using_OpenCV/e_kids.png')
+    print('Embedding Done')
+    string = extract('C:/Users/DELL/OneDrive/Desktop/Stegnanography/Stegnanography_using_OpenCV/e_kids.png')
+    print(string)
 
 main()
